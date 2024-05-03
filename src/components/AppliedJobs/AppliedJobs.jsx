e21c81014676376eff6a7221bed5aa41f6eae06e
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-05-03 21:30:44 +0600

    added navbar and handling errorPage

---
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+
+
+const AppliedJobs = () => {
+    return (
+        <div>
+            <h3 className="text-3xl bg-red-400 p-2 text-black m-20">Jobs I applied...</h3>
+        </div>
+    );
+};
+
+export default AppliedJobs;
---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+
+const ErrorPage = () => {
+    return (
+        <div className="bg-red-700 text-4xl text-white font-semibold p-20 text-center">
+            <h1 className="pb-16">Oops!!</h1>
+            <h2>The page you search is not found...</h2>
+            <Link to="/"><button className="bg-orange-500 hover:bg-orange-600 text-black p-2 rounded-xl text-lg m-32">Back To Home</button></Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
