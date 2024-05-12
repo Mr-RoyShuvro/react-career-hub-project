a851943f588eadb7197bf735b28561e8cf86f573
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-05-12 11:56:25 +0600

    Completed CareerHub Project

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,7 +8,7 @@ const Banner = () => {
                 <div>
                     <h1 className="text-5xl font-bold leading-normal">One Step <br /> Closer to your <br /><span className="text-sky-500">Dream Job</span></h1>
                     <p className="py-6">Explore thousands of job opportunies with all the <br />information you need. Its your future. Come find it. Manage all <br />your job application from start to finish.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <button className="btn btn-primary bg-sky-500 hover:bg-sky-700 text-white font-medium">Get Started</button>
                 </div>
             </div>
         </div>
